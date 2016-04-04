9e5e962780fcd669438807e445412c34c4a074e2
Konradkelc <Kelc.konrad@hotmail.com>
2016-04-04 19:32:14 +0200

    Added files via upload

---
+++ client/build/generated/cb/Server.java
@@ -0,0 +1,13 @@
+package cb;
+
+/**
+ * Generated from IDL interface "Server".
+ *
+ * @author JacORB IDL compiler V 3.7
+ * @version generated at Apr 4, 2016 2:41:02 PM
+ */
+
+public interface Server
+	extends ServerOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity
+{
+}
